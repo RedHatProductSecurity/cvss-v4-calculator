80a6d4d7a2012a4c54e30dc58babaa1d1dada695
Stanislav Kontar <skontar@redhat.com>
2023-01-11 16:39:59 +0100

    Add draft computations

---
+++ cvss_lookup.js
@@ -0,0 +1,722 @@
+cvssLookup = {
+  "0": {
+    "base_score": "10",
+    "qual_score": "Critical"
+  },
+  "10000": {
+    "base_score": "9.9",
+    "qual_score": "Critical"
+  },
+  "1": {
+    "base_score": "9.9",
+    "qual_score": "Critical"
+  },
+  "2000": {
+    "base_score": "9.9",
+    "qual_score": "Critical"
+  },
+  "10": {
+    "base_score": "9.8",
+    "qual_score": "Critical"
+  },
+  "100": {
+    "base_score": "9.8",
+    "qual_score": "Critical"
+  },
+  "100000": {
+    "base_score": "9.7",
+    "qual_score": "Critical"
+  },
+  "10001": {
+    "base_score": "9.7",
+    "qual_score": "Critical"
+  },
+  "12000": {
+    "base_score": "9.6",
+    "qual_score": "Critical"
+  },
+  "10010": {
+    "base_score": "9.6",
+    "qual_score": "Critical"
+  },
+  "10100": {
+    "base_score": "9.6",
+    "qual_score": "Critical"
+  },
+  "2010": {
+    "base_score": "9.6",
+    "qual_score": "Critical"
+  },
+  "2001": {
+    "base_score": "9.6",
+    "qual_score": "Critical"
+  },
+  "11": {
+    "base_score": "9.6",
+    "qual_score": "Critical"
+  },
+  "101": {
+    "base_score": "9.5",
+    "qual_score": "Critical"
+  },
+  "20": {
+    "base_score": "9.5",
+    "qual_score": "Critical"
+  },
+  "100001": {
+    "base_score": "9.4",
+    "qual_score": "Critical"
+  },
+  "110": {
+    "base_score": "9.4",
+    "qual_score": "Critical"
+  },
+  "110000": {
+    "base_score": "9.4",
+    "qual_score": "Critical"
+  },
+  "12001": {
+    "base_score": "9.4",
+    "qual_score": "Critical"
+  },
+  "2100": {
+    "base_score": "9.4",
+    "qual_score": "Critical"
+  },
+  "200": {
+    "base_score": "9.4",
+    "qual_score": "Critical"
+  },
+  "10020": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "102000": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "21": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "2101": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "100100": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "10200": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "100010": {
+    "base_score": "9.3",
+    "qual_score": "Critical"
+  },
+  "12100": {
+    "base_score": "9.2",
+    "qual_score": "Critical"
+  },
+  "10011": {
+    "base_score": "9.2",
+    "qual_score": "Critical"
+  },
+  "3001": {
+    "base_score": "9.2",
+    "qual_score": "Critical"
+  },
+  "12010": {
+    "base_score": "9.2",
+    "qual_score": "Critical"
+  },
+  "10101": {
+    "base_score": "9.2",
+    "qual_score": "Critical"
+  },
+  "201": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "120": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "2011": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "2020": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "2110": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "10110": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "210": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "111": {
+    "base_score": "9.1",
+    "qual_score": "Critical"
+  },
+  "100020": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "2200": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "10021": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "13001": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "12020": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "110001": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "12011": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "12200": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "2021": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "110100": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "112000": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "10111": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "10120": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "102001": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "102010": {
+    "base_score": "9",
+    "qual_score": "Critical"
+  },
+  "100101": {
+    "base_score": "8.9",
+    "qual_score": "High"
+  },
+  "2111": {
+    "base_score": "8.9",
+    "qual_score": "High"
+  },
+  "12101": {
+    "base_score": "8.8",
+    "qual_score": "High"
+  },
+  "2120": {
+    "base_score": "8.8",
+    "qual_score": "High"
+  },
+  "121": {
+    "base_score": "8.8",
+    "qual_score": "High"
+  },
+  "110010": {
+    "base_score": "8.7",
+    "qual_score": "High"
+  },
+  "3101": {
+    "base_score": "8.7",
+    "qual_score": "High"
+  },
+  "100110": {
+    "base_score": "8.6",
+    "qual_score": "High"
+  },
+  "10210": {
+    "base_score": "8.6",
+    "qual_score": "High"
+  },
+  "100011": {
+    "base_score": "8.5",
+    "qual_score": "High"
+  },
+  "220": {
+    "base_score": "8.5",
+    "qual_score": "High"
+  },
+  "10201": {
+    "base_score": "8.5",
+    "qual_score": "High"
+  },
+  "12110": {
+    "base_score": "8.5",
+    "qual_score": "High"
+  },
+  "3011": {
+    "base_score": "8.5",
+    "qual_score": "High"
+  },
+  "100200": {
+    "base_score": "8.4",
+    "qual_score": "High"
+  },
+  "211": {
+    "base_score": "8.3",
+    "qual_score": "High"
+  },
+  "103001": {
+    "base_score": "8.3",
+    "qual_score": "High"
+  },
+  "2201": {
+    "base_score": "8.3",
+    "qual_score": "High"
+  },
+  "102100": {
+    "base_score": "8.2",
+    "qual_score": "High"
+  },
+  "2210": {
+    "base_score": "8.1",
+    "qual_score": "High"
+  },
+  "13011": {
+    "base_score": "8",
+    "qual_score": "High"
+  },
+  "100021": {
+    "base_score": "7.9",
+    "qual_score": "High"
+  },
+  "3021": {
+    "base_score": "7.9",
+    "qual_score": "High"
+  },
+  "102101": {
+    "base_score": "7.8",
+    "qual_score": "High"
+  },
+  "102011": {
+    "base_score": "7.7",
+    "qual_score": "High"
+  },
+  "110020": {
+    "base_score": "7.7",
+    "qual_score": "High"
+  },
+  "100120": {
+    "base_score": "7.6",
+    "qual_score": "High"
+  },
+  "110101": {
+    "base_score": "7.5",
+    "qual_score": "High"
+  },
+  "12021": {
+    "base_score": "7.5",
+    "qual_score": "High"
+  },
+  "12111": {
+    "base_score": "7.5",
+    "qual_score": "High"
+  },
+  "112010": {
+    "base_score": "7.5",
+    "qual_score": "High"
+  },
+  "102020": {
+    "base_score": "7.4",
+    "qual_score": "High"
+  },
+  "10220": {
+    "base_score": "7.3",
+    "qual_score": "High"
+  },
+  "112100": {
+    "base_score": "7.3",
+    "qual_score": "High"
+  },
+  "112001": {
+    "base_score": "7.2",
+    "qual_score": "High"
+  },
+  "10121": {
+    "base_score": "7.2",
+    "qual_score": "High"
+  },
+  "3201": {
+    "base_score": "7.2",
+    "qual_score": "High"
+  },
+  "110200": {
+    "base_score": "7.1",
+    "qual_score": "High"
+  },
+  "110011": {
+    "base_score": "7.1",
+    "qual_score": "High"
+  },
+  "12201": {
+    "base_score": "7",
+    "qual_score": "High"
+  },
+  "100201": {
+    "base_score": "6.9",
+    "qual_score": "Medium"
+  },
+  "10211": {
+    "base_score": "6.8",
+    "qual_score": "Medium"
+  },
+  "13101": {
+    "base_score": "6.8",
+    "qual_score": "Medium"
+  },
+  "100111": {
+    "base_score": "6.7",
+    "qual_score": "Medium"
+  },
+  "110110": {
+    "base_score": "6.6",
+    "qual_score": "Medium"
+  },
+  "12120": {
+    "base_score": "6.5",
+    "qual_score": "Medium"
+  },
+  "2211": {
+    "base_score": "6.4",
+    "qual_score": "Medium"
+  },
+  "12210": {
+    "base_score": "6.4",
+    "qual_score": "Medium"
+  },
+  "113001": {
+    "base_score": "6.4",
+    "qual_score": "Medium"
+  },
+  "102110": {
+    "base_score": "6.3",
+    "qual_score": "Medium"
+  },
+  "221": {
+    "base_score": "6.3",
+    "qual_score": "Medium"
+  },
+  "2121": {
+    "base_score": "6.3",
+    "qual_score": "Medium"
+  },
+  "3111": {
+    "base_score": "6.2",
+    "qual_score": "Medium"
+  },
+  "100210": {
+    "base_score": "6.2",
+    "qual_score": "Medium"
+  },
+  "2220": {
+    "base_score": "6.2",
+    "qual_score": "Medium"
+  },
+  "103101": {
+    "base_score": "6.1",
+    "qual_score": "Medium"
+  },
+  "102200": {
+    "base_score": "6",
+    "qual_score": "Medium"
+  },
+  "103011": {
+    "base_score": "5.9",
+    "qual_score": "Medium"
+  },
+  "112011": {
+    "base_score": "5.8",
+    "qual_score": "Medium"
+  },
+  "110021": {
+    "base_score": "5.7",
+    "qual_score": "Medium"
+  },
+  "110201": {
+    "base_score": "5.7",
+    "qual_score": "Medium"
+  },
+  "112020": {
+    "base_score": "5.6",
+    "qual_score": "Medium"
+  },
+  "102021": {
+    "base_score": "5.6",
+    "qual_score": "Medium"
+  },
+  "110210": {
+    "base_score": "5.6",
+    "qual_score": "Medium"
+  },
+  "12211": {
+    "base_score": "5.5",
+    "qual_score": "Medium"
+  },
+  "13111": {
+    "base_score": "5.5",
+    "qual_score": "Medium"
+  },
+  "100211": {
+    "base_score": "5.4",
+    "qual_score": "Medium"
+  },
+  "112101": {
+    "base_score": "5.3",
+    "qual_score": "Medium"
+  },
+  "10221": {
+    "base_score": "5.2",
+    "qual_score": "Medium"
+  },
+  "2221": {
+    "base_score": "5.2",
+    "qual_score": "Medium"
+  },
+  "100220": {
+    "base_score": "5.2",
+    "qual_score": "Medium"
+  },
+  "12220": {
+    "base_score": "5.1",
+    "qual_score": "Medium"
+  },
+  "110111": {
+    "base_score": "5.1",
+    "qual_score": "Medium"
+  },
+  "102201": {
+    "base_score": "5",
+    "qual_score": "Medium"
+  },
+  "13021": {
+    "base_score": "4.9",
+    "qual_score": "Medium"
+  },
+  "3121": {
+    "base_score": "4.8",
+    "qual_score": "Medium"
+  },
+  "112200": {
+    "base_score": "4.7",
+    "qual_score": "Medium"
+  },
+  "3211": {
+    "base_score": "4.6",
+    "qual_score": "Medium"
+  },
+  "102210": {
+    "base_score": "4.6",
+    "qual_score": "Medium"
+  },
+  "102120": {
+    "base_score": "4.5",
+    "qual_score": "Medium"
+  },
+  "113101": {
+    "base_score": "4.4",
+    "qual_score": "Medium"
+  },
+  "112110": {
+    "base_score": "4.4",
+    "qual_score": "Medium"
+  },
+  "102111": {
+    "base_score": "4.3",
+    "qual_score": "Medium"
+  },
+  "12121": {
+    "base_score": "4.2",
+    "qual_score": "Medium"
+  },
+  "100121": {
+    "base_score": "4.1",
+    "qual_score": "Medium"
+  },
+  "113011": {
+    "base_score": "4",
+    "qual_score": "Medium"
+  },
+  "110120": {
+    "base_score": "4",
+    "qual_score": "Medium"
+  },
+  "13201": {
+    "base_score": "3.9",
+    "qual_score": "Low"
+  },
+  "103201": {
+    "base_score": "3.8",
+    "qual_score": "Low"
+  },
+  "103021": {
+    "base_score": "3.7",
+    "qual_score": "Low"
+  },
+  "112201": {
+    "base_score": "3.6",
+    "qual_score": "Low"
+  },
+  "112021": {
+    "base_score": "3.5",
+    "qual_score": "Low"
+  },
+  "110220": {
+    "base_score": "3.4",
+    "qual_score": "Low"
+  },
+  "100221": {
+    "base_score": "3.2",
+    "qual_score": "Low"
+  },
+  "110211": {
+    "base_score": "3.1",
+    "qual_score": "Low"
+  },
+  "113021": {
+    "base_score": "3",
+    "qual_score": "Low"
+  },
+  "110121": {
+    "base_score": "2.9",
+    "qual_score": "Low"
+  },
+  "103111": {
+    "base_score": "2.8",
+    "qual_score": "Low"
+  },
+  "102121": {
+    "base_score": "2.7",
+    "qual_score": "Low"
+  },
+  "12221": {
+    "base_score": "2.6",
+    "qual_score": "Low"
+  },
+  "112120": {
+    "base_score": "2.5",
+    "qual_score": "Low"
+  },
+  "13121": {
+    "base_score": "2.4",
+    "qual_score": "Low"
+  },
+  "13211": {
+    "base_score": "2.3",
+    "qual_score": "Low"
+  },
+  "112111": {
+    "base_score": "2.2",
+    "qual_score": "Low"
+  },
+  "102211": {
+    "base_score": "2.1",
+    "qual_score": "Low"
+  },
+  "102220": {
+    "base_score": "1.9",
+    "qual_score": "Low"
+  },
+  "112210": {
+    "base_score": "1.8",
+    "qual_score": "Low"
+  },
+  "3221": {
+    "base_score": "1.7",
+    "qual_score": "Low"
+  },
+  "113111": {
+    "base_score": "1.6",
+    "qual_score": "Low"
+  },
+  "103121": {
+    "base_score": "1.5",
+    "qual_score": "Low"
+  },
+  "113201": {
+    "base_score": "1.4",
+    "qual_score": "Low"
+  },
+  "112211": {
+    "base_score": "1.3",
+    "qual_score": "Low"
+  },
+  "110221": {
+    "base_score": "1.2",
+    "qual_score": "Low"
+  },
+  "102221": {
+    "base_score": "1.1",
+    "qual_score": "Low"
+  },
+  "103211": {
+    "base_score": "1",
+    "qual_score": "Low"
+  },
+  "112121": {
+    "base_score": "0.9",
+    "qual_score": "Low"
+  },
+  "112220": {
+    "base_score": "0.8",
+    "qual_score": "Low"
+  },
+  "13221": {
+    "base_score": "0.6",
+    "qual_score": "Low"
+  },
+  "113121": {
+    "base_score": "0.5",
+    "qual_score": "Low"
+  },
+  "113211": {
+    "base_score": "0.4",
+    "qual_score": "Low"
+  },
+  "112221": {
+    "base_score": "0.3",
+    "qual_score": "Low"
+  },
+  "103221": {
+    "base_score": "0.2",
+    "qual_score": "Low"
+  },
+  "113221": {
+    "base_score": "0.1",
+    "qual_score": "Low"
+  }
+}
