7728bd107fd4402b12c44b331e7cc2ef84faeddf
Stanislav Kontar <skontar@redhat.com>
2023-01-16 19:32:48 +0100

    Add mising config for vector details

---
+++ cvss_details.js
@@ -0,0 +1,14 @@
+cvssMacroVectorDetails = {
+  "Exploitability": 0,
+  "Complexity": 1,
+  "Vulnerable system": 2,
+  "Subsequent system": 3,
+  "Exploitation": 4,
+  "Security requirements": 5
+}
+
+cvssMacroVectorValues = {
+  "0": "High",
+  "1": "Medium",
+  "2": "Low",
+}
