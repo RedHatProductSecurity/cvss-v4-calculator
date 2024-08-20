92b77017e6cc4f6b5fc576ac7b04b712ecfe1f88
n3rada <72791564+n3rada@users.noreply.github.com>
2024-08-20 14:14:14 +0000

    Add comments

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+// Copyright FIRST, Red Hat, and contributors
+// SPDX-License-Identifier: BSD-2-Clause
+
 const app = Vue.createApp({
     data() {
         return {
             cvssConfigData: null, // Initialize as null, will be populated after loading JSON
-            expectedMetricOrder: CVSS40.EXPECTED_METRIC_ORDER,
-            showDetails: false,
-            vectorMetrics: {},  // Initialize as an empty object
-            header_height: 0,
-            macroVector: null,
-            cvssInstance: null
+            expectedMetricOrder: CVSS40.EXPECTED_METRIC_ORDER, // Expected order of metrics as per CVSS 4.0
+            showDetails: false, // Controls the display of detailed metric information
+            vectorMetrics: {},  // Initialize as an empty object to hold selected metric values
+            header_height: 0, // To store the height of the header, useful for responsive design
+            macroVector: null, // Placeholder for a summarized vector representation
+            cvssInstance: null // Placeholder for the CVSS40 class instance
         }
     },
     methods: {
@@ -77,20 +80,27 @@ const app = Vue.createApp({
                 const [key, value] = metrics[index].split(":");
                 let expected = Object.entries(this.expectedMetricOrder)[oi++];
                 while (true) {
+                    // If out of possible metrics ordering, it not a valid value thus
+                    // the vector is invalid
                     if (expected === undefined) {
                         console.log("Error invalid vector, too many metric values");
                         return;
                     }
                     if (key !== expected[0]) {
+                        // If not this metric but is mandatory, the vector is invalid
+                        // As the only mandatory ones are from the Base group, 11 is the
+                        // number of metrics part of it.
                         if (oi <= 11) {
                             console.log("Error invalid vector, missing mandatory metrics");
                             return;
                         }
+                        // If a non-mandatory, retry
                         expected = Object.entries(this.expectedMetricOrder)[oi++];
                         continue;
                     }
                     break;
                 }
+                // The value MUST be part of the metric's values, case insensitive
                 if (!expected[1].includes(value)) {
                     console.log(`Error invalid vector, for key ${key}, value ${value} is not in ${expected[1]}`);
                     return;
@@ -99,14 +109,18 @@ const app = Vue.createApp({
                     toSelect[key] = value;
                 }
             }
-
+            
+            // Apply if it is compliant
             for (const key in toSelect) {
                 this.vectorMetrics[key] = toSelect[key];
             }
+            // Recalculate the CVSS instance with the updated data
             this.updateCVSSInstance();
         },
         updateCVSSInstance() {
+            // Create a new instance of CVSS40
             this.cvssInstance = new CVSS40(this.vector);
+            // Update the macro vector result
             this.macroVector = this.cvssInstance.macroVectorResult;
         },
         onReset() {
@@ -117,6 +131,7 @@ const app = Vue.createApp({
         resetSelected() {
             this.vectorMetrics = {};
             if (this.cvssConfigData) {
+                // Loop through the cvssConfigData to set the default selections
                 for (const [metricType, metricTypeData] of Object.entries(this.cvssConfigData)) {
                     for (const [metricGroup, metricGroupData] of Object.entries(metricTypeData.metric_groups)) {
                         for (const [metric, metricData] of Object.entries(metricGroupData)) {
